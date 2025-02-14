ee1048778da3cf98fbb27ba1af7a63e141d3ea0e
KGandhi90 <kevin.gandhi@somaiya.edu>
2025-02-14 23:56:27 +0530

    New Example Added in UseEffect

--- Hooks/useEffect/src/main.jsx
+++ Hooks/useEffect/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Example1 from './Example1'
+import Example2 from './Example2'
 
 createRoot(document.getElementById('root')).render(
   // Strict Mode runs the code twice on the very first render
   // Reason : 1st time it checks the code and 2nd time it actually runs the code
   <StrictMode>
-    <Example1 />
+    {/* <Example1 /> */}
+    <Example2 />
   </StrictMode>,
 )
